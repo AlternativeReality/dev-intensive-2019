bcb5b540957babba4ba5b285929516ebf8388a84
AlternativeReality <56733577+AlternativeReality@users.noreply.github.com>
2020-02-28 16:44:56 +0300

    hometask_4

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -9,36 +9,25 @@ import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
 
-fun Activity.hideKeyboard() {
-    val inputManager: InputMethodManager =
-        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    inputManager.hideSoftInputFromWindow(currentFocus.windowToken, InputMethodManager.SHOW_FORCED)
+//fun Activity.hideKeyboard() {
+//    val inputManager: InputMethodManager =
+//        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//    inputManager.hideSoftInputFromWindow(currentFocus.windowToken, InputMethodManager.SHOW_FORCED)
+//
+//}
 
-}
-
-fun Activity.isKeyboardOpen(): Boolean {
-    val r = Rect()
-    rootView.getWindowVisibleDisplayFrame(r)
-    val keyBoard = rootView.height - (r.bottom - r.top)
-    return (keyBoard > 128)
-    //{
-//        return true
-//    } else {
-//        return false
-//    }
-//Toast.makeText(this, "клавиатура открыта", Toast.LENGTH_SHORT).show()
-//            Log.d("M_MainActivity","клавиатура открыта")
-
-
-//    Log.d("M_MainActivity", "я работаю в конце")
-
-}
-fun Activity.isKeyboardClosed(): Boolean{
-    val r = Rect()
-    rootView.getWindowVisibleDisplayFrame(r)
-    val keyBoard = rootView.height - (r.bottom - r.top)
-    return (keyBoard < 128)
-}
+//fun Activity.isKeyboardOpen(): Boolean {
+//    val r = Rect()
+//    rootView.getWindowVisibleDisplayFrame(r)
+//    val keyBoard = rootView.height - (r.bottom - r.top)
+//    return (keyBoard > 128)
+//}
+//fun Activity.isKeyboardClosed(): Boolean{
+//    val r = Rect()
+//    rootView.getWindowVisibleDisplayFrame(r)
+//    val keyBoard = rootView.height - (r.bottom - r.top)
+//    return (keyBoard < 128)
+//}
